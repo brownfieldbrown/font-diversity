cfd8b7b1f1b6d5c488f605ce0582c9b67bf2d98a
brownfieldbrown <55472237+brownfieldbrown@users.noreply.github.com>
2019-12-19 11:58:20 -0600

    Add files via upload

--- js/loadpage.js
+++ js/loadpage.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 
-	setTimeout(() => { window.location.replace("Hello_Language.html"); }, 12000); // "3000" is the amount of time before the next page loads in milliseconds.
+	setTimeout(() => { window.location.replace("hello_language.html"); }, 12000); // "3000" is the amount of time before the next page loads in milliseconds.
 
 });
