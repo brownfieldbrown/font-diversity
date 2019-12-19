dc3fafafa1310572a42ab52e22ca2c5ceeda03ce
brownfieldbrown <55472237+brownfieldbrown@users.noreply.github.com>
2019-12-19 11:58:52 -0600

    Delete loadpage2.js

--- js/loadpage2.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-
-	setTimeout(() => { window.location.replace("hello_mainscreen.html"); }, 30000); // "3000" is the amount of time before the next page loads in milliseconds.
-
-});
